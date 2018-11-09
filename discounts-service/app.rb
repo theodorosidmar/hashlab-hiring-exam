4324b56b412396a37268a6569ee825700584ea69
Sidmar Theodoro <theodorosidmar@gmail.com>
2018-11-09 00:47:47 -0200

    [FEAT] Applying discount accordingly to user birth_date

--- discounts-service/app.rb
+++ discounts-service/app.rb
@@ -9,12 +9,36 @@ require 'discount_services_pb'
 class DiscountServer < Discount::Service::Service
   def get(request, call)
     puts "Request received: #{request.inspect}"
-    puts "His birth_date: #{Time.at(request.birth_date.to_i/1000)}"
-    products = request.products.to_a
-    products.each do |product|
-      product['discount'] = Discount::Discount.new(pct: 1, value_in_cents: 10000)
+    raise GRPC::BadStatus.new(GRPC::Core::StatusCodes::INVALID_ARGUMENT, "Bad request") unless
+      request.birth_date > 0 and
+      request.products.length > 0
+    begin
+      now = Time.now
+      products = request.products.to_a
+      birth_date = Time.at(request.birth_date.to_i / 1000)
+      discount = 0
+      if birth_date.day == 29 and birth_date.month == 02
+        # No discount for February 29
+        return Discount::GetResponse.new(products: products)
+      elsif (birth_date.day == now.day) && (birth_date.month == now.month)
+        # 50% discount for his birthday
+        discount = 0.50
+      elsif birth_date.month == now.month
+        # 20% discount for his month birthday
+        discount = 0.20
+      end
+      if discount > 0
+        products.each do |product|
+          price_in_cents = product.price_in_cents
+          value_in_cents = price_in_cents - (price_in_cents * (1 - discount)).to_i
+          product['discount'] = Discount::Discount.new(pct: 1, value_in_cents: value_in_cents)
+        end
+      end
+      Discount::GetResponse.new(products: products)
+    rescue Exception => error
+      p "Error: #{error.inspect}"
+      raise GRPC::BadStatus.new(GRPC::Core::StatusCodes::UNKNOWN, "Unknown")
     end
-    Discount::GetResponse.new(products: products)
   end
 end
 
@@ -24,7 +48,7 @@ def main
     server = GRPC::RpcServer.new
     server.add_http2_port("#{ENV['HOST']}:#{ENV['PORT']}", :this_port_is_insecure)
     puts "Server running insecurely on #{ENV['HOST']}:#{ENV['PORT']}"
-    server.handle(DiscountServer.new)
+    server.handle(DiscountServer)
     server.run_till_terminated
   rescue SystemExit, Interrupt
     puts "Server stopped"
