2256ad04d19dc22c8a9d29ad275124ccc910456d
Sidmar Theodoro <theodorosidmar@gmail.com>
2018-11-29 13:57:14 -0200

    [CHORE] Comments

--- products-service/src/discount/service.js
+++ products-service/src/discount/service.js
@@ -3,6 +3,7 @@ const protoLoader = require('@grpc/proto-loader')
 const path = require('path')
 
 const discountServiceUrl = `${process.env.DISCOUNTS_SERVICE_HOST}:${process.env.DISCOUNTS_SERVICE_PORT}`
+// Workaround for this test (protoPath)
 const protoPath = process.env.NODE_ENV === 'production' ?
   path.join(__dirname, '../protos', 'discount.proto') :
   path.join(__dirname, '../../../', 'protos', 'discount.proto')
